bb7a66118ef343988f7eb23252917faf60a7d615
ssc <qwertyuiop>
2021-05-31 09:46:35 +0800

    fix: 子表单没有必填选项的问题

--- packages/config/dynamic.vue
+++ packages/config/dynamic.vue
@@ -54,12 +54,42 @@
     <el-form-item label="是否可见">
       <el-switch v-model="data.display"></el-switch>
     </el-form-item>
+    <el-form-item label="是否必填">
+      <el-switch v-model="data.required"></el-switch>
+    </el-form-item>
   </div>
 </template>
 
 <script>
 export default {
   name: "config-dynamic",
-  props: ['data']
+  props: ['data'],
+  data() {
+    return {
+      validator: {
+        type: null,
+        required: null,
+        pattern: null,
+        length: null
+      }
+    }
+  },
+  methods: {
+    generateRule() {
+      const rules = [];
+      Object.keys(this.validator).forEach(key => {
+        if (this.validator[key]) rules.push(this.validator[key])
+      })
+      this.data.rules = rules
+    },
+  },
+  watch: {
+    'data.required': function (val) {
+      if (val) this.validator.required = { required: true, message: `${this.data.label}必须填写` }
+      else this.validator.required = null
+
+      this.generateRule()
+    },
+  }
 }
 </script>
