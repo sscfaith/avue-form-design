c90e64f8d38f878a06ef0d2736752a1b04c9deaf
ssc <273702440@qq.com>
2022-03-03 09:15:41 +0800

    perf: 区分build与lib打包方式

--- vite.config.js
+++ vite.config.js
@@ -3,27 +3,31 @@ const path = require('path')
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 
-// https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [vue()],
-  build: {
-    cssCodeSplit: true,
-    outDir: 'lib',
-    lib: {
-      entry: path.resolve(__dirname, 'packages/index.js'),
-      name: 'AvueFormDesign',
-      formats: ['umd'],
-      fileName: (format) => `index.${format}.js`
-    },
-    rollupOptions: {
-      // 确保外部化处理那些你不想打包进库的依赖
-      external: ['vue', 'element-plus'],
-      output: {
-        inlineDynamicImports: true,
-        globals: {
-          vue: 'Vue'
+export default defineConfig(({ mode }) => {
+  const config = {
+    plugins: [vue()]
+  }
+  if (mode == 'lib') {
+    config.build = {
+      cssCodeSplit: true,
+      outDir: 'lib',
+      lib: {
+        entry: path.resolve(__dirname, 'packages/index.js'),
+        name: 'AvueFormDesign',
+        formats: ['umd'],
+        fileName: (format) => `index.${format}.js`
+      },
+      rollupOptions: {
+        // 确保外部化处理那些你不想打包进库的依赖
+        external: ['vue', 'element-plus'],
+        output: {
+          inlineDynamicImports: true,
+          globals: {
+            vue: 'Vue'
+          }
         }
       }
     }
   }
+  return config
 })
